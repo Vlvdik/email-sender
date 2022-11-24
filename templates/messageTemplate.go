64b82048521e938979c609fae1d8a69e7eb4bcb8
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-11-24 23:39:32 +0300

    Create messageTemplate.go

---
+++ templates/messageTemplate.go
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body style="background: #280441; text-align: center;">
+    <h1 style="color:#ad8f8f;"> Hello, {{.Name}} {{.LastName}}!</h1>
+    <h3>I know that your birthday date is {{.Birthday}} </h3>
+    <p>Im waiting this celly also like you!</p>
+</body>
+</html>
