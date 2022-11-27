6e90337237391c6431607c55375a6bd974daaa78
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-11-27 21:57:02 +0300

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 
 func main() {
 	sd, es := app.Init()
-	app.FinishJobs(sd)
+	go app.FinishJobs(sd)
 	ctx := context.Background()
 	go func() {
 		err := es.Start()
@@ -21,32 +21,34 @@ func main() {
 	}()
 	defer app.Close(&es, ctx)
 
-	fmt.Println("Привет! Чтобы отослать рассылку напиши 1, чтобы отослать рассылку через промежуток времени - 2, чтобы выйти - 0")
+	fmt.Println("\nHi! To send a newsletter write 1, to send a deferred newsletter write 2, to see a list of deferred newsletters write 3, to exit write 0.")
 	for {
 		var input string
-		fmt.Println("Выберите команду: ")
+		fmt.Println("\nSelect the command: ")
 		_, err := fmt.Scan(&input)
 		if err != nil && err != io.EOF {
 			log.Fatal(err)
 		}
 
-		if input == "1" {
+		switch input {
+		case "1":
 			app.SendMails(sd)
-
-		} else if input == "2" {
+		case "2":
 			var duration time.Duration
-			fmt.Println("Укажите задержку в секундах: ")
+			fmt.Println("\nSpecify the delay in seconds: ")
 			_, err = fmt.Scan(&duration)
 			if err != nil && err != io.EOF {
 				log.Fatal(err)
 			}
 
 			go app.SendMailsWithDuration(sd, duration*time.Second)
-			fmt.Printf("Задача будет выполнена через %v секунд\n", duration)
-		} else if input == "0" {
-			break
-		} else {
-			fmt.Println("Неизвестная команда")
+			fmt.Printf("\nThe task will be completed in %v seconds\n", duration)
+		case "3":
+			app.GetJobs()
+		case "0":
+			return
+		default:
+			fmt.Println("\nUnknown command")
 		}
 	}
 }
