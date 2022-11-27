0a5eb1d5ed92f2165c19a66385c59a3998860865
Vlvdik <114529141+Vlvdik@users.noreply.github.com>
2022-11-27 03:30:07 +0300

    Update app.go

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,12 +2,114 @@ package app
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"mail-Sender/config"
-	"mail-Sender/server"
+	"mail-Sender/pkg/server"
 	"net/smtp"
 	"time"
 )
+
+type Jobs []struct {
+	Date     time.Time     `json:"job_date"`
+	Duration time.Duration `json:"duration"`
+}
+
+type Job struct {
+	Date     time.Time     `json:"job_date"`
+	Duration time.Duration `json:"duration"`
+}
+
+func writeJob(job Job) {
+	dataIn, err := ioutil.ReadFile("pkg/app/config/jobs.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jobs Jobs
+	err = json.Unmarshal(dataIn, &jobs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jobs = append(jobs, job)
+
+	dataOut, err := json.MarshalIndent(&jobs, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("pkg/app/config/jobs.json", dataOut, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func deleteJob(job Job) {
+	dataIn, err := ioutil.ReadFile("pkg/app/config/jobs.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jobs Jobs
+	err = json.Unmarshal(dataIn, &jobs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(jobs) > 1 {
+		for idx, value := range jobs {
+			if value.Date.Format("02-Jan-2006 15:04:05") == job.Date.Format("02-Jan-2006 15:04:05") {
+				jobs[idx] = jobs[len(jobs)-1]
+				jobs = jobs[:len(jobs)-1]
+			}
+		}
+	} else {
+		jobs = Jobs{}
+	}
+
+	dataOut, err := json.MarshalIndent(&jobs, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("pkg/app/config/jobs.json", dataOut, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func FinishJobs(sd config.SendData) {
+	dataIn, err := ioutil.ReadFile("pkg/app/config/jobs.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jobs Jobs
+	err = json.Unmarshal(dataIn, &jobs)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(jobs) > 0 {
+		stamp := time.Now()
+		for _, value := range jobs {
+			if value.Date.Add(value.Duration).Before(stamp) {
+				SendMails(sd)
+				deleteJob(value)
+			} else {
+				currentDuration := value.Date.Add(value.Duration).Sub(stamp)
+
+				SendMailsWithDuration(sd, currentDuration)
+				deleteJob(value)
+
+				fmt.Printf("Выполню чуть позже, через %v секунд", currentDuration)
+			}
+		}
+	}
+}
 
 func Init() (config.SendData, server.EmailServer) {
 	var s config.Sender
@@ -48,9 +150,11 @@ func SendMails(sd config.SendData) {
 func SendMailsWithDuration(sd config.SendData, duration time.Duration) {
 	auth := sd.From.Auth()
 	address := sd.From.Host + ":" + sd.From.Port
-
 	stamp := time.Now()
-	time.Sleep(duration * time.Second)
+	job := Job{Date: stamp, Duration: duration}
+
+	writeJob(job)
+	time.Sleep(job.Duration)
 
 	for _, value := range sd.To {
 		sd.Data.SetSubjectPersonInfo(value.PersonalInfo.Name, value.PersonalInfo.LastName)
@@ -61,7 +165,7 @@ func SendMailsWithDuration(sd config.SendData, duration time.Duration) {
 			log.Fatal(err)
 		}
 	}
-
+	deleteJob(job)
 	log.Printf("Отложенная рассылка успешно выполнена!\n Дата и время задачи: %v\n", stamp.Format("02-Jan-2006 15:04:05"))
 }
 
